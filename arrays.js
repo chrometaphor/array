6329c005d6cc586fb931bba1cd7dd7260322e530
Chris Cole <christopher.a.cole@gmail.com>
2022-11-09 12:07:49 -0800

    add notes and methods to the script

--- arrays.js
+++ arrays.js
@@ -1,3 +1,18 @@
-var students = ["Cindy", "Josiah", "Rigo", "Billy"];
+var students = ["Crimby", "Bucifelous", "Caroline", "Shmilly"];
 var tickets = [88, 68, 98, 99];
-var online = [true, false, false true]
+var online = [true, false, false, true]
+
+// this line calls the pop method on the students array. Pop will  remove the last element from an array and returns that element.
+students.pop();
+console.log(students);
+
+// this line calls the shift method on the tickets array. Shift removes the first element from an array and returns that removed element.
+tickets.shift();
+console.log(tickets);
+
+// this line calls the push method on the line array. Push adds one or more elements to the end of an array and returns the new length of the array.
+online.push();
+console.log(online);
+
+
+
